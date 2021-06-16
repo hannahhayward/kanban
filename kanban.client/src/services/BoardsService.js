0c7def58a44c1b163652e034b6a93c2c963dc2ea
hannah hayward <hannahhayward97@gmail.com>
2021-06-16 11:14:29 -0600

    before merge

--- kanban.client/src/services/BoardsService.js
+++ kanban.client/src/services/BoardsService.js
@@ -1,4 +1,15 @@
-class BoardsService {
+import { logger } from '../utils/Logger'
+import { api } from './AxiosService'
+import Notification from '../utils/Notification'
 
+class BoardsService {
+  async createBoard(newBoard) {
+    try {
+      const res = await api.post('api/boards', newBoard)
+      logger.log(res.data)
+    } catch (error) {
+      Notification.toast(error, error)
+    }
+  }
 }
 export const boardsService = new BoardsService()
