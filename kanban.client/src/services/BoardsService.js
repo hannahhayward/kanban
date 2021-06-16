8142752f77a333fd599256b79df2e6d8be1ec270
hannah hayward <hannahhayward97@gmail.com>
2021-06-16 14:49:31 -0600

    fixed

--- kanban.client/src/services/BoardsService.js
+++ kanban.client/src/services/BoardsService.js
@@ -1,12 +1,24 @@
 import { AppState } from '../AppState'
 import { logger } from '../utils/Logger'
 import { api } from './AxiosService'
+import Notification from '../utils/Notification'
 
 class BoardsService {
   async getBoards() {
     const res = await api.get('/api/boards')
     AppState.boards = res.data
     logger.log(res)
+  }
+
+  async createBoard(newBoard) {
+    const account = AppState.account.id
+    try {
+      const res = await api.post(`/api/boards/${account}`, newBoard)
+      AppState.boards = res.data
+      logger.log(AppState.boards, 'appstate')
+    } catch (error) {
+      Notification.toast(error, error)
+    }
   }
 }
 export const boardsService = new BoardsService()
