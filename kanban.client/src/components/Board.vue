195678fa5726692444d20c46b163b33f921a43f7
hannah hayward <hannahhayward97@gmail.com>
2021-06-17 20:00:15 -0600

    boards page styling

--- kanban.client/src/components/Board.vue
+++ kanban.client/src/components/Board.vue
@@ -1,22 +1,20 @@
 <template>
-  <div class="card w-100 my-3 text-light"
+  <div class="w-100 text-light col-5 m-2 bg shadow  b"
        :style="{
          backgroundColor: board.color,
          '-webkit-filter': true
        }"
   >
-    <div class="card-header">
-      <i class="fas fa-times fa-2x text-right text-dark" @click="deleteBoard(board.id)"></i>
+    <div class="header">
+      <i class="fas fa-times fa-2x text-right text-dark p-1 shadow" @click="deleteBoard(board.id)"></i>
     </div>
-    <div class="card-body">
+    <div class="body">
       <!-- TODO - figure out how to remove text decoration -->
-      <router-link :to="{ name: 'Board', params:{id: board.id} }" :key="board.boardId" style="text-decoration: none" class=" link">
+      <router-link :to="{ name: 'Board', params:{id: board.id} }" :key="board.boardId" style="text-decoration: none" class=" text-light">
         <h1 class="text-center text">
           {{ board.name }}
         </h1>
       </router-link>
-    </div>
-    <div>
     </div>
   </div>
 </template>
@@ -50,4 +48,12 @@ export default {
 .link{
   text-decoration: none;
 }
+.bg{
+  background-color: #e2e2df;
+  outline: solid .5px; color: #dfdfd5;
+}
+.b{
+  border: rgb(252, 255, 249) solid 1px;
+}
+
 </style>
